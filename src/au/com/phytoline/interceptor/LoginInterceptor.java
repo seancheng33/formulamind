0c2cf96582b57cb3bc2b5c041b249317133dfafc
sean <aya234@163.com>
2017-08-17 15:06:04 +0800

    添加登录认证的拦截器

--- src/au/com/phytoline/interceptor/LoginInterceptor.java
+++ src/au/com/phytoline/interceptor/LoginInterceptor.java
@@ -6,6 +6,8 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
+import au.com.phytoline.entity.User;
+
 public class LoginInterceptor extends AbstractInterceptor {
 
 	private static final long serialVersionUID = 1L;
@@ -13,8 +15,12 @@ public class LoginInterceptor extends AbstractInterceptor {
 	@Override
 	public String intercept(ActionInvocation invocation) throws Exception {
 		ActionContext actionContext = invocation.getInvocationContext();
-		if(actionContext != null){
-			return "login";
+		Map<String, Object> session =actionContext.getSession();
+		User users = (User) session.get("LoginUser");
+		//System.out.println(users.getUname());
+		
+		if(users != null){
+			return invocation.invoke();
 		}
 		return "error";
 	}
